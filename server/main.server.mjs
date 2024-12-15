7ad24972c4aeb453100fd1176169ec942935a2cc
Aneeshkumarsl <50114850+Aneeshkumarsl@users.noreply.github.com>
2024-12-15 12:52:36 +0530

    deploy build

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BBUGK6H3.mjs";import"./chunk-XTNLBOWG.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-WVIKUINT.mjs";import"./chunk-XTNLBOWG.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
